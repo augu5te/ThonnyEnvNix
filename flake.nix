e6cf7a96ecdc50f2fb9758fca0cfb1a05c5c7dca
Olivier Richard <olivier.richard@imag.fr>
2024-09-08 14:55:29 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Thonny with env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: (utils.lib.eachSystem ["x86_64-linux" ] (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system}; 
+    in
+    rec {
+      packages = {
+        thonnyEnv = let
+          pythonEnv = with pkgs.python3Packages; [
+            matplotlib
+            numpy
+            pandas
+          ];
+        in
+          pkgs.mkShell {
+            buildInputs = [ pkgs.thonny ] ++ pythonEnv;
+            shellHook = ''echo Thonny dev'';
+          };
+      };
+
+      defaultPackage = packages.thonnyEnv;
+      devShell = packages.thonnyEnv;
+    }));
+}
